ea383b226a371dc4b211bad2dae7f3b1d6e74184
masoudparsa <m.parsanejad@gmail.com>
2016-06-14 12:04:32 +0430

    test

---
+++ MVCHelloWorldTypeScript/Scripts/Api/Car.js
@@ -0,0 +1,20 @@
+﻿var Car = (function () {
+    function Car(engine) {
+        this.engine = engine;
+    }
+    Car.prototype.Start = function () {
+        alert('Engine started: ' + this.engine);
+    };
+
+    Car.prototype.Stop = function () {
+        alert('Engine stopped: ' + this.engine);
+    };
+    return Car;
+})();
+
+window.onload = function () {
+    var car = new Car('V8');
+    car.Start();
+    car.Stop();
+};
+//# sourceMappingURL=Car.js.map
